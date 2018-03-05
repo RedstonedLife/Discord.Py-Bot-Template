192ed843dc0021e4d21b84a9cad91ec19b988e5c
RedstonedLife <37072229+RedstonedLife@users.noreply.github.com>
2018-03-05 19:06:25 +0200

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import requests
+import asyncio
+import config
+
+bot = commands.Bot(command_prefix='Bot Prefix (Example: b;)')
+
+@bot.command(pass_context=True)
+async def info(ctx):
+  embed=discord.Embed(color=0xff7171)
+  embed.add_field(name="Python", value="3.6.4", inline=True)
+  embed.add_field(name="discord.py", value="1.0.0a", inline=True)
+  embed.add_field(name="About BlueTemp", value="This is an instance of BlueTemp, an open sorce Discord Bot created by RedstonedLife", inline=False)
+  embed.set_footer(text="A Template downloaded from Github")
+  await ctx.send(embed=embed)
+
+@bot.event
+async def on_ready():
+  print("Logged in as: " + bot.user.name + " " + "With the id: " + bot.user.id)
+  print("---------------------------------------------------------------------")
+
